169271bdf91231b4f5f80839fb6b58a0e0f978b2
ghost <samuelbrownai@gmail.com>
2024-03-01 23:00:28 +0000

    author unit test dao

--- src/main/java/com/niiad/summer/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/niiad/summer/dao/impl/AuthorDaoImpl.java
@@ -1,6 +1,7 @@
 package com.niiad.summer.dao.impl;
 
 import com.niiad.summer.dao.AuthorDao;
+import com.niiad.summer.domain.Author;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AuthorDaoImpl implements AuthorDao {
@@ -8,5 +9,13 @@ public class AuthorDaoImpl implements AuthorDao {
 
     public AuthorDaoImpl(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Author author) {
+        jdbcTemplate.update(
+                "INSERT INTO authors (id, name, age) VALUES (?, ?, ?)",
+                author.getId(), author.getName(), author.getAge()
+        );
     }
 }
