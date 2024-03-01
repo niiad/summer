b3ed389a01536c7665b24bd081109bf07957a49d
ghost <samuelbrownai@gmail.com>
2024-03-01 13:51:28 +0000

    jdbc

--- src/main/java/com/niiad/summer/SummerApplication.java
+++ src/main/java/com/niiad/summer/SummerApplication.java
@@ -1,13 +1,32 @@
 package com.niiad.summer;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class SummerApplication {
+@Log
+public class SummerApplication implements CommandLineRunner {
+	private final DataSource dataSource;
+
+	public SummerApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SummerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		log.info("Datasource: " + dataSource.toString());
+
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+
+		restTemplate.execute("SELECT 1");
+	}
 }
