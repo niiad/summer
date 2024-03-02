75433fbe66d7cdfa2f13919a6d2c2e5a148fc0c0
ghost <samuelbrownai@gmail.com>
2024-03-02 09:26:18 +0000

    book read test

--- src/main/java/com/niiad/summer/dao/BookDao.java
+++ src/main/java/com/niiad/summer/dao/BookDao.java
@@ -2,7 +2,10 @@ package com.niiad.summer.dao;
 
 import com.niiad.summer.domain.Book;
 
+import java.util.Optional;
+
 public interface BookDao {
     void create(Book book);
 
+    Optional<Book> find(String isbn);
 }
--- src/test/java/com/niiad/summer/dao/BookDaoImplTests.java
+++ src/test/java/com/niiad/summer/dao/BookDaoImplTests.java
@@ -4,11 +4,13 @@ import com.niiad.summer.dao.impl.BookDaoImpl;
 import com.niiad.summer.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
@@ -35,4 +37,16 @@ public class BookDaoImplTests {
                 eq(book.getIsbn()), eq(book.getTitle()), eq(book.getAuthorId())
         );
     }
+
+    @Test
+    public void testThatFindOneBookGeneratesCorrectSql() {
+        underTest.find("978-1-2345-6789-0");
+
+        verify(jdbcTemplate).query(
+                eq("SELECT isbn, title, author_id from books WHERE isbn = ? LIMIT 1"),
+                ArgumentMatchers.<BookDaoImpl.BookRowMapper>any(),
+                eq("978-1-2345-6789-0")
+        );
+    }
+
 }
